c4d784bdabef794e399ce687196be58312ec973c
Witold Wysota <wysota@wysota.org>
2017-07-14 23:30:43 +0200

    Redesign of the tracing tool

--- cpp/mlcommon/include/tracing/tracing.h
+++ cpp/mlcommon/include/tracing/tracing.h
@@ -1,19 +1,194 @@
 #ifndef TRACING_H
 #define TRACING_H
 
+#include <QCoreApplication>
 #include <QFile>
 #include <QMutex>
 #include <QString>
 #include <QTextStream>
+#include <QThreadStorage>
+#include <QVector>
 #include <chrono>
 #include <sys/file.h>
+#include <thread>
 
 using namespace std::chrono;
 
-//class TracingEntry {
-//public:
-//    TracingEntry(char type)
-//};
+namespace Trace {
+
+
+/*
+{
+  "name": "myName",
+  "cat": "category,list",
+  "ph": "B",
+  "ts": 12345,
+  "pid": 123,
+  "tid": 456,
+  "args": {
+    "someArg": 1,
+    "anotherArg": {
+      "value": "my value"
+    }
+  }
+}
+*/
+
+class Event {
+public:
+    typedef uint32_t Pid;
+    typedef uint64_t Tid;
+    typedef int64_t Timestamp;
+    Event(Timestamp ts, Tid tid = 0, Pid pid = 0) : m_pid(pid), m_tid(tid), m_ts(ts) {
+        if (m_pid == 0)
+            m_pid = QCoreApplication::applicationPid();
+        if (m_tid == 0) {
+            static std::hash<std::thread::id> hasher;
+            m_tid = hasher(std::this_thread::get_id());
+        }
+    }
+    virtual ~Event(){}
+    virtual char eventType() const = 0;
+    QString name() const;
+    void setName(const QString &name);
+
+    QStringList cat() const;
+    void setCat(const QStringList &cat);
+
+    Timestamp ts() const;
+    void setTs(const Timestamp &ts);
+
+    Timestamp tts() const;
+    void setTts(const Timestamp &tts);
+
+    Pid pid() const;
+    void setPid(const Pid &pid);
+
+    Tid tid() const;
+    void setTid(const Tid &tid);
+
+    QVariantMap args() const;
+    void setArgs(const QVariantMap &args);
+
+    QString cname() const;
+    void setCname(const QString &cname);
+
+    virtual void serialize(QJsonObject &doc) const;
+protected:
+    QVariantMap& argsRef() { return m_args; }
+private:
+    QString m_name;
+    QStringList m_cat;
+    Pid m_pid;
+    Tid m_tid;
+    Timestamp m_ts;
+    Timestamp m_tts;
+    QVariantMap m_args;
+    QString m_cname;
+};
+
+class DurationEvent : public Event {
+public:
+    DurationEvent(Tid tid, Timestamp ts, Pid pid = 0)
+        : Event(tid, ts, pid){}
+};
+
+class DurationEventB : public DurationEvent {
+public:
+    DurationEventB(const QString& name, Tid tid, Timestamp ts,  Pid pid = 0)
+        : DurationEvent(tid, ts, pid){
+        setName(name);
+    }
+    virtual char eventType() const override { return 'B'; }
+};
+
+class DurationEventE : public DurationEvent {
+public:
+    DurationEventE(Tid tid, Timestamp ts, Pid pid = 0)
+        : DurationEvent(tid, ts, pid){}
+    virtual char eventType() const override { return 'E'; }
+};
+
+class CounterEvent : public Event {
+public:
+    CounterEvent(const QString& name, Tid tid, Timestamp ts, Pid pid = 0)
+        : Event(tid, ts, pid){
+        setName(name);
+    }
+    void setValue(const QString& series, QVariant value) {
+        argsRef().insert(series, value);
+    }
+    QVariant value(const QString& series) const {
+        return args().value(series, QVariant());
+    }
+    void setId(int id) {
+        m_id = id;
+        m_idSet = true;
+    }
+    int id() const { return m_id; }
+
+    virtual char eventType() const override { return 'C'; }
+    void serialize(QJsonObject &json) const;
+private:
+    int m_id = 0;
+    bool m_idSet = false;
+};
+
+class EventManager {
+public:
+    EventManager() {}
+    ~EventManager() {
+        flush();
+    }
+    void append(Event* e) {
+        m_events << e;
+        if (m_events.size() > 100)
+            flush();
+    }
+    void flush();
+protected:
+//    void doFlush(const QVector<Event*>& events);
+
+private:
+    QVector<Event*> m_events;
+};
+
+class TracingSystem {
+public:
+    TracingSystem() {
+        if (m_instance) {
+            qWarning("TracingSystem instance already present");
+            return;
+        }
+        m_instance = this;
+        init();
+    }
+    ~TracingSystem() {
+        if (m_instance == this) m_instance = nullptr;
+    }
+    EventManager& eventManager() { return m_manager.localData(); }
+    static TracingSystem* instance() { return m_instance; }
+protected:
+    void init();
+    void initMaster();
+    void initSlave();
+    void doFlush(const QVector<Event*>& events);
+    void flush(QVector<Event*> events);
+
+private:
+    QString m_traceFilePath;
+    static TracingSystem* m_instance;
+    QThreadStorage<EventManager> m_manager;
+    QMutex m_mutex;
+    friend class EventManager;
+};
+
+class TraceApi {
+public:
+
+};
+
+} // namespace Trace
 
 class LockedFile : public QFile {
 public:
@@ -21,6 +196,7 @@ public:
         SharedLock,
         ExclusiveLock
     };
+
     void lock(LockType lt) {
         flock(handle(), lt == SharedLock ? LOCK_SH : LOCK_EX);
     }
--- cpp/mlcommon/src/tracing/tracing.cpp
+++ cpp/mlcommon/src/tracing/tracing.cpp
@@ -6,6 +6,24 @@
 #include <QJsonObject>
 #include <thread>
 
+namespace Trace {
+TracingSystem* TracingSystem::m_instance = nullptr;
+
+void EventManager::flush() {
+    if (m_events.isEmpty()) return;
+    QVector<Event*> eventsToFlush;
+    qSwap(eventsToFlush, m_events);
+    TracingSystem::instance()->flush(eventsToFlush);
+    //        // do flush
+    //        // options:
+    //        // 1. flush synchronously
+    //        // 2. spawn a thread and flush there
+    //        // 3. have a global per-pid thread that handles flush
+    //        qDeleteAll(eventsToFlush);
+}
+
+}
+
 Tracing* Tracing::m_instance = nullptr;
 
 Tracing::Tracing(const QString& filePath)
@@ -99,6 +117,7 @@ void Tracing::open()
 {
     QMutexLocker locker(&m_mutex);
     m_file.setFileName(m_path);
+//    qgetenv("TRACE")
     m_file.open(QIODevice::WriteOnly|QIODevice::Truncate);
     m_stream.setDevice(&m_file);
     m_stream << "[" << endl;
@@ -108,3 +127,179 @@ void Tracing::write(const QJsonObject &entry)
 {
     m_stream << "  " << QJsonDocument(entry).toJson(QJsonDocument::Compact) << "," << endl;
 }
+
+namespace Trace {
+QString Event::name() const
+{
+    return m_name;
+}
+
+void Event::setName(const QString &name)
+{
+    m_name = name;
+}
+
+QStringList Event::cat() const
+{
+    return m_cat;
+}
+
+void Event::setCat(const QStringList &cat)
+{
+    m_cat = cat;
+}
+
+Event::Timestamp Event::ts() const
+{
+    return m_ts;
+}
+
+void Event::setTs(const Event::Timestamp &ts)
+{
+    m_ts = ts;
+}
+
+Event::Timestamp Event::tts() const
+{
+    return m_tts;
+}
+
+void Event::setTts(const Event::Timestamp &tts)
+{
+    m_tts = tts;
+}
+
+Event::Pid Event::pid() const
+{
+    return m_pid;
+}
+
+void Event::setPid(const Event::Pid &pid)
+{
+    m_pid = pid;
+}
+
+Event::Tid Event::tid() const
+{
+    return m_tid;
+}
+
+void Event::setTid(const Event::Tid &tid)
+{
+    m_tid = tid;
+}
+
+QVariantMap Event::args() const
+{
+    return m_args;
+}
+
+void Event::setArgs(const QVariantMap &args)
+{
+    m_args = args;
+}
+
+QString Event::cname() const
+{
+    return m_cname;
+}
+
+void Event::setCname(const QString &cname)
+{
+    m_cname = cname;
+}
+
+void Event::serialize(QJsonObject &json) const
+{
+    json.insert("name", name());
+    // cats
+    if (cat().isEmpty())
+        json.insert("cat", cat().join(','));
+    json.insert("ph", eventType());
+    json.insert("pid", (qint64)pid());
+    json.insert("tid", (qint64)tid());
+    json.insert("ts", (qint64)ts());
+    if (!cname().isEmpty())
+        json.insert("cname", cname());
+    json.insert("args", QJsonObject::fromVariantMap(args()));
+}
+
+void CounterEvent::serialize(QJsonObject &json) const {
+    Event::serialize(json);
+    if (m_idSet)
+        json.insert("id", id());
+}
+
+void TracingSystem::init()
+{
+    QByteArray ba = qgetenv("TRACE_MASTER");
+    if (ba.isEmpty()) {
+        qputenv("TRACE_MASTER", QByteArray::number(QCoreApplication::applicationPid()));
+        initMaster();
+    } else {
+        initSlave();
+    }
+}
+
+void TracingSystem::initMaster()
+{
+    // tell slaves about the file
+    const QStringList& args = QCoreApplication::arguments();
+    for (int i = 1; i < args.size(); ++i) {
+        if (args[i].startsWith("--trace-file=")) {
+            m_traceFilePath = args[i].mid(13);
+            continue;
+        }
+        if (args[i] == "--trace-file") {
+            if (i+1 < args.size()) {
+                m_traceFilePath = args[++i];
+            }
+            continue;
+        }
+    }
+    qputenv("TRACE_FILE", m_traceFilePath.toLocal8Bit());
+    LockedFile file;
+    file.lock(LockedFile::ExclusiveLock);
+    file.open(QIODevice::WriteOnly|QIODevice::Truncate|QIODevice::Text);
+    file.write("[\n");
+    file.close();
+    file.unlock();
+}
+
+void TracingSystem::initSlave()
+{
+    m_traceFilePath = qgetenv("TRACE_FILE");
+}
+
+void TracingSystem::doFlush(const QVector<Event *> &events)
+{
+    QMutexLocker locker(&m_mutex);
+    LockedFile file;
+    file.lock(LockedFile::ExclusiveLock);
+    file.open(QIODevice::Append|QIODevice::Text);
+    QTextStream stream(&file);
+    foreach(Event* e, events) {
+        QJsonObject obj;
+        e->serialize(obj);
+        stream << "  " << QJsonDocument(obj).toJson(QJsonDocument::Compact) << ',' << endl;
+    }
+    file.close();
+    file.unlock();
+}
+
+void TracingSystem::flush(QVector<Event *> events)
+{
+    doFlush(events);
+    qDeleteAll(events);
+}
+
+
+//void EventManager::doFlush(const QVector<Event *> &events) {
+//    foreach(Event* e, events) {
+//        QJsonObject obj;
+//        e->serialize(obj);
+//    }
+//}
+
+}
+
